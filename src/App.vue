672ce42d5c400c09903fd90b59f2b307867fb497
Aleksandr <soft@vhlv.ru>
2023-03-31 01:44:48 +0300

    feat: add html structure

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,57 @@
 <template>
-  <div></div>
+  <main class="main">
+    <div class="building">
+      <div class="elevators">
+        <ul
+          v-for="shaft in numberOfShafts"
+          :key="shaft"
+          class="elevators__shaft"
+        >
+          <li
+            v-for="floor in floors"
+            :key="floor"
+            class="elevators__shaft-item"
+          ></li>
+          <div
+            class="elevator"
+            style="bottom: 0"
+          ></div>
+        </ul>
+      </div>
+      <ul class="floors">
+        <li
+          v-for="floor in floors"
+          :key="floor"
+          class="floor"
+        >
+          <div class="floor__container">
+            <span class="floor__number">
+              {{ floor }}
+            </span>
+            <button class="floor__button">
+              <span class="floor__button-indicator"></span>
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </main>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      numberOfFloors: 5,
+      numberOfShafts: 1
+    };
+  },
+  computed: {
+    floors() {
+      return Array.from({ length: this.numberOfFloors }, (_, index) => index + 1).reverse();
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
